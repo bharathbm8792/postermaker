7a4d49a56687523da49482b4aa688725da5d5b90
Bharath <bharathbm@integramicro.co.in>
2025-02-22 16:21:38 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/missingpetsbangalore', // Set the base path for your application
+  base: '/missingpetsbangalore/', // Set the base path for your application
 })
