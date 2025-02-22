f07b5f547cd2059acc20032c879dadef43484d82
Bharath <bharathbm@integramicro.co.in>
2025-02-22 16:01:34 +0530

    Commit

--- src/components/MainPage/DashBoard.jsx
+++ src/components/MainPage/DashBoard.jsx
@@ -1,12 +1,17 @@
 import styles from './Dashboard.module.css';
 import { useNavigate } from "react-router-dom";
+import logo from '/public/logo.jpg'
 
 function DashBoard() {
     const navigate = useNavigate();
 
     return (
         <div className={styles.overallContainer}>
+            <p>Version  {import.meta.env.VITE_APP_VER} </p>
+            <img src={logo} alt='Missing pets Bangalore' style={{ width: "100px", height: "100px", marginBottom: "20px" }} />
+
             <div className={styles.container}>
+
                 <button onClick={() => navigate('/createposter')}>
                     Create Poster
                 </button>
